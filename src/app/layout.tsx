0cb3d8c6161f53d2a75b7225fd521a1196847e16
Soligann <prepheadrus@gmail.com>
2025-11-30 16:19:30 +0000

    senden ilk istediğim şeyleri hayata geçirelim. benimle türkçe konuş lütf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="tr" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -27,7 +27,10 @@ export default function RootLayout({
         />
       </head>
       <body className="font-body antialiased">
-        {children}
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className="text-4xl font-bold">MechQuote</h1>
+            <p>Mekanik Teklif Otomasyon Sistemi</p>
+        </main>
         <Toaster />
       </body>
     </html>
