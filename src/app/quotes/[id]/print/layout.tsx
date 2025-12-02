76c162e0943fa4f84cf4f343a4a103adc9fc8cd6
Soligann <prepheadrus@gmail.com>
2025-12-02 17:37:02 +0000

    Try fixing this error: `Console Error: In HTML, <body> cannot be a child

--- src/app/quotes/[id]/print/layout.tsx
+++ src/app/quotes/[id]/print/layout.tsx
@@ -3,9 +3,5 @@ export default function PrintLayout({
   }: {
     children: React.ReactNode;
   }) {
-    return (
-      <body className="font-body antialiased bg-white">
-            {children}
-      </body>
-    );
+    return <>{children}</>;
   }
