e952d12ec002997bdc2770615562ba4a3e5bba64
Soligann <prepheadrus@gmail.com>
2025-12-04 19:30:42 +0000

    çıktıyı şimdi görüyorum. yerleşim olarak güzel görünüyor. ancak sol üstt

--- src/app/quotes/[id]/print/layout.tsx
+++ src/app/quotes/[id]/print/layout.tsx
@@ -1,7 +1,21 @@
+import "@/app/globals.css";
+
 export default function PrintLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return <>{children}</>;
-  }
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="tr">
+       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
