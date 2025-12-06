2a8a05354d9c6e659769d5d850c035d1baf233a3
Soligann <prepheadrus@gmail.com>
2025-12-06 20:16:42 +0000

    Oluşturulan tüm print bileşenlerini src/components/app/index.ts dosyasın

---
+++ src/components/app/index.ts
@@ -0,0 +1,12 @@
+// Generic App Components
+export * from './quick-add-customer';
+export * from './quick-add-product';
+export * from './quick-add-installation-type';
+export * from './product-selector';
+
+// Print-specific Components
+export * from './printable-quote';
+export * from './quote-header';
+export * from './quote-customer-info';
+export * from './quote-items-table';
+export * from './quote-footer';
